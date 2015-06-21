6a1125826d4bb8dd0ca1a314a68d493016997d3c
kasei_san <kasei.san@gmail.com>
2015-06-21 23:07:45 +0900

    jsonに結果を出力

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'twitter'
 require 'yaml'
+require 'json'
 require File.join(__dir__, 'entry.rb')
 
 credential =  YAML.load_file('config/credential.yml')
@@ -19,10 +20,11 @@ valid, invalid = search_results.partition do |tweet|
   Entry.search_entry(tweet.full_text)
 end
 
-result = Hash.new([])
+result = {}
 
 # 無効票
-result[:invalid] = invalid.map do |tweet|
+result[:invalid] = {}
+result[:invalid][:no_member] = invalid.map do |tweet|
   {
     name: tweet.user.screen_name,
     text: tweet.full_text,
@@ -32,37 +34,43 @@ result[:invalid] = invalid.map do |tweet|
 end
 
 # 有効票
+result[:valid] = {}
+result[:invalid][:multi_vote] = []
 users = Hash.new(0)
 entry_counter = Hash.new(0)
 valid.each do |tweet|
-
   users[tweet.user.screen_name] += 1
-  if users[tweet.user.screen_name] > 2
-    result[:invald] << {
+  if users[tweet.user.screen_name] > 2 # 2票以上の投票
+    result[:invalid][:multi_vote] << {
+      name: tweet.user.screen_name,
+      text: tweet.full_text,
+      icon: tweet.user.profile_image_url_https.to_s,
+      url: "https://twitter.com/#{tweet.user.screen_name}/status/#{tweet.id}"
+    }
+  else
+    entry_id = Entry.search_entry(tweet.full_text)
+    entry_counter[entry_id] += 1
+    result[:valid][entry_id] ||= []
+    result[:valid][entry_id] << {
       name: tweet.user.screen_name,
       text: tweet.full_text,
       icon: tweet.user.profile_image_url_https.to_s,
       url: "https://twitter.com/#{tweet.user.screen_name}/status/#{tweet.id}"
     }
   end
+end
 
-  entry_id = Entry.search_entry(tweet.full_text)
-  entry_counter[entry_id] += 1
-  result[:valid][entry_id] ||= []
-  result[:valid][entry_id] << {
-    name: tweet.user.screen_name,
-    text: tweet.full_text,
-    icon: tweet.user.profile_image_url_https.to_s,
-    url: "https://twitter.com/#{tweet.user.screen_name}/status/#{tweet.id}"
+result[:rank] = []
+
+members = %w[マディソンおばあちゃん アンクル・ストマック船長 イビルヤモト 大男 キツネ・ウエスギ卿 カラルド ミスター・ウィルキンソン  ザザ ハワード・キングスレイ(ストライカーのヘンドリクス役のひと フィメール・ザ・ヴァーティゴ 逆噴射聡一郎 こそつき(魚が好き) シーライフのあれ ブロンディ 死と虚無の王(覚悟が必要)]
+result[:rank] = entry_counter.sort{|a,b| a[1]<=>b[1]}.each_with_index.map do |(k,v), i|
+  {
+    rank: i,
+    name: members[k-1],
+    vortes: v
   }
 end
 
-members = %w[マディソンおばあちゃん アンクル・ストマック船長 イビルヤモト 大男 キツネ・ウエスギ卿 カラルド ミスター・ウィルキンソン  ザザ ハワード・キングスレイ(ストライカーのヘンドリクス役のひと フィメール・ザ・ヴァーティゴ 逆噴射聡一郎 こそつき(魚が好き) シーライフのあれ ブロンディ 死と虚無の王(覚悟が必要)]
-entry_counter.sort{|a,b| a[1]<=>b[1]}.each_with_index do |(k,v), i|
-  puts "#{15-i}位 : #{members[k-1]} : #{v}票"
-end
-
-puts "==========無効票=========="
-result[:invalid].each do |tweet|
-  puts tweet.inspect
+File.open(File.join(__dir__, 'tmp', 'result.json'), 'w') do |file|
+  JSON.dump(result, file)
 end
