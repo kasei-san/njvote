48247f68b74512a492218f4882d16d9b4d1e58c3
kasei_san <kasei.san@gmail.com>
2015-06-22 00:05:59 +0900

    Rename tmp to json

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require 'json'
 require File.join(__dir__, 'entry.rb')
 
-credential =  YAML.load_file('config/credential.yml')
+credential =  YAML.load_file('config/credential.yml') # ここに認証情報入れる
 client = Twitter::REST::Client.new(credential)
 search_results = client.search('#njvoteV -rt', lang: 'ja')
 
@@ -65,12 +65,13 @@ result[:rank] = []
 members = %w[マディソンおばあちゃん アンクル・ストマック船長 イビルヤモト 大男 キツネ・ウエスギ卿 カラルド ミスター・ウィルキンソン  ザザ ハワード・キングスレイ(ストライカーのヘンドリクス役のひと フィメール・ザ・ヴァーティゴ 逆噴射聡一郎 こそつき(魚が好き) シーライフのあれ ブロンディ 死と虚無の王(覚悟が必要)]
 result[:rank] = entry_counter.sort{|a,b| a[1]<=>b[1]}.each_with_index.map do |(k,v), i|
   {
-    rank: i,
+    id: k,
+    rank: 15-i,
     name: members[k-1],
-    vortes: v
+    votes: v
   }
 end
 
-File.open(File.join(__dir__, 'tmp', 'result.json'), 'w') do |file|
+File.open(File.join(__dir__, 'json', 'result.json'), 'w') do |file|
   JSON.dump(result, file)
 end
